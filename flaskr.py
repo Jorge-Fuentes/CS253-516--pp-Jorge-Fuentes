3d7dbae8b26e6f27c00ac2764b075f546b3022b1
Mark Liffiton <liffiton@gmail.com>
2016-10-12 14:56:47 -0500

    Add ability to give each post a category.

--- flaskr.py
+++ flaskr.py
@@ -71,7 +71,7 @@ def close_db(error):
 @app.route('/')
 def show_entries():
     db = get_db()
-    cur = db.execute('select title, text from entries order by id desc')
+    cur = db.execute('select title, category, text from entries order by id desc')
     entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
 
@@ -81,8 +81,8 @@ def add_entry():
     if not session.get('logged_in'):
         abort(401)
     db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-               [request.form['title'], request.form['text']])
+    db.execute('insert into entries (title, category, text) values (?, ?, ?)',
+               [request.form['title'], request.form['category'], request.form['text']])
     db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
