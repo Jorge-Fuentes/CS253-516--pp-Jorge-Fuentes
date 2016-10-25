59c62d86cc99da5c0fb9476d7f927e3c69efe4ee
Jorge-Fuentes <jfuentes@iwu.edu>
2016-10-24 20:33:07 -0500

    1st test
    See's if entries there thus far.

--- flaskr_test.py
+++ flaskr_test.py
@@ -16,5 +16,19 @@ class FlaskrTestCase(unittest.TestCase):
         os.close(self.db_fd)
         os.unlink(flaskr.app.config['DATABASE'])
 
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+        self.app = flaskr.app.test_client()
+        flaskr.init_db()
+
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(flaskr.app.config['DATABASE'])
+
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert b'No entries here so far' in rv.data
 if __name__ == '__main__':
     unittest.main()
