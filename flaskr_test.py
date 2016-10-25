be202c8e13113edcf1d055ffba0c9a7a659ca533
Jorge-Fuentes <jfuentes@iwu.edu>
2016-10-24 20:56:13 -0500

    Login/logout test
    Test to see if login/logout work added.

--- flaskr_test.py
+++ flaskr_test.py
@@ -3,18 +3,6 @@ import flaskr
 import unittest
 import tempfile
 
-class FlaskrTestCase(unittest.TestCase):
-
-    def setUp(self):
-        self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-        flaskr.app.config['TESTING'] = True
-        self.app = flaskr.app.test_client()
-        with flaskr.app.app_context():
-            flaskr.init_db()
-
-    def tearDown(self):
-        os.close(self.db_fd)
-        os.unlink(flaskr.app.config['DATABASE'])
 
 class FlaskrTestCase(unittest.TestCase):
 
@@ -30,5 +18,19 @@ class FlaskrTestCase(unittest.TestCase):
     def test_empty_db(self):
         rv = self.app.get('/')
         assert b'No entries here so far' in rv.data
+        
+    def login(self, username, password):
+    return self.app.post('/login', data=dict(
+        username=username,
+        password=password
+    ), follow_redirects=True)
+
+    def logout(self):
+    return self.app.get('/logout', follow_redirects=True)
+
+    
+
+
+
 if __name__ == '__main__':
     unittest.main()
